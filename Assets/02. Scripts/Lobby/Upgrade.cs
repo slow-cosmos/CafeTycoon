b41d32ec99761eb19551c94415ba9054297467a4
slow-cosmos <inki1030@naver.com>
2023-09-18 23:40:59 +0900

    #1 UI 리소스 교체

--- Assets/02. Scripts/Lobby/Upgrade.cs
+++ Assets/02. Scripts/Lobby/Upgrade.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
 
 public class Upgrade : MonoBehaviour
 {
+    [SerializeField] private RectTransform gradeRect;
+    [SerializeField] private Image[] grade;
+    [SerializeField] private Sprite[] gradeSprite;
     [SerializeField] private TMP_Text name;
     [SerializeField] private Image image;
     [SerializeField] private TMP_Text current;
@@ -20,10 +24,25 @@ public class Upgrade : MonoBehaviour
         name.text = upgradeInfo.name;
 
         image.sprite = upgradeInfo.sprite;
-        image.rectTransform.sizeDelta = new Vector3(upgradeInfo.sprite.bounds.size.x*100, upgradeInfo.sprite.bounds.size.y*100, 0);
+        image.rectTransform.sizeDelta = new Vector3(upgradeInfo.sprite.bounds.size.x * 100, upgradeInfo.sprite.bounds.size.y * 100, 0);
         image.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
 
-        switch(upgradeInfo.upgradeType)
+        switch (upgradeInfo.upgrade.Count)
+        {
+            case 3:
+                gradeRect.sizeDelta = new Vector2(70, 40);
+                break;
+            case 4:
+                gradeRect.sizeDelta = new Vector2(90, 40);
+                break;
+        }
+
+        for (int i = 0; i < upgradeInfo.upgrade.Count; i++)
+        {
+            grade[i].gameObject.SetActive(true);
+        }
+
+        switch (upgradeInfo.upgradeType)
         {
             case UpgradeType.Count:
                 type.text = "생산(보관)수량증가";
@@ -43,6 +62,15 @@ public class Upgrade : MonoBehaviour
     {
         int curUpgrade = UpgradeManager.Instance.GetCurUpgradeValue(key);
         current.text = upgradeInfo.upgrade[curUpgrade].ToString();
-        next.text = curUpgrade+1 == upgradeInfo.upgrade.Count ? upgradeInfo.upgrade[curUpgrade].ToString() : upgradeInfo.upgrade[curUpgrade+1].ToString();
+        next.text = curUpgrade + 1 == upgradeInfo.upgrade.Count ? upgradeInfo.upgrade[curUpgrade].ToString() : upgradeInfo.upgrade[curUpgrade + 1].ToString();
+
+        for (int i = 0; i <= curUpgrade; i++) // grade 표시
+        {
+            grade[i].sprite = gradeSprite[1];
+        }
+        for (int i = curUpgrade + 1; i < upgradeInfo.upgrade.Count; i++)
+        {
+            grade[i].sprite = gradeSprite[0];
+        }
     }
 }
